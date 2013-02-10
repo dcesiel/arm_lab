89d79713427555d7fe0758e300d00074113b1030
dcesiel <dcesiel@umich.edu>
2013-02-10 17:39:30 -0500

    StateMachine now subscribes to lcmmessages and loads actual_angles array

--- java/src/StateMachine.java
+++ java/src/StateMachine.java
@@ -19,9 +19,9 @@ import april.*;
 // Should take in a location an angle and distance and outputs an array //
 // of six angle values for the arm                                      //
 //======================================================================//
-public class StateMachine
+public class StateMachine implements LCMSubscriber
 {
-    
+
     //Arm Length Constants
     static double L1 = 12; //7.5cm + 4.5cm for Base + Pivot1
     static double L2 = 10.5;
@@ -29,16 +29,17 @@ public class StateMachine
     static double L4 = 8.0;
     //This doesn't seem right but I'm just going by the dims
     static double L5 = 19.5; //8 + 2 + 8.5 + height of claw above the board
-    
+
     //Gripper Constants
     static double GRIPPER_OPEN = -30.0;
     static double GRIPPER_CLOSED = 0.0;
-    
+
     double armSubBase;
     double L2Sq;
     double L3Sq;
-    
+
     double angles[] = new double[6];
+    double actual_angles[] = new double[6];
     ConstraintCheck cc = new ConstraintCheck();
 
     public StateMachine()
@@ -50,24 +51,24 @@ public class StateMachine
             angles[i] = 0;
         }
     }
-    
+
     protected void loadAngles(double angle, double BaseToL2, double L2ToL3, double Wrist){
         angles[0] = angle;
         angles[1] = BaseToL2;
-        angles[2] = L2ToL3; 
+        angles[2] = L2ToL3;
         angles[3] = Wrist;
     }
-    
+
     protected void openGripper() {
         angles[5] = GRIPPER_OPEN;
         //TODO: Add some feedback that checks when the ball is actually open
     }
-    
+
     protected void closeGripper() {
         angles[5] = GRIPPER_CLOSED;
-        //TODO: Add some feedback that checks when gripper is actually closed    
+        //TODO: Add some feedback that checks when gripper is actually closed
     }
-    
+
     public void pickUp90(double angle, double armDistance){
         //Do angle calculations (this is explained in the README)
 
@@ -94,9 +95,9 @@ public class StateMachine
         openGripper();
         cc.check(angles);
     }
-    
+
     public void pickUpStraight(double angles){}
-    
+
     //======================================================================//
     // ballPickUp()                                                         //
     // Determins how far a ball is away from the arm. Depending on it's     //
@@ -128,4 +129,19 @@ public class StateMachine
 		}*/
 	}
 
+
+    @Override
+    public void messageReceived(LCM lcm, String channel, LCMDataInputStream dins)
+    {
+        try {
+            dynamixel_status_list_t arm_status = new dynamixel_status_list_t(dins);
+            /* access positions using arm_status.statuses[i].position_radians */
+            for (int i = 0; i < 6; i++){
+                actual_angles[i] = arm_status.statuses[i].position_radians;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
