5d3eaa66e3acb36082230cc910ffe53d87fb5e13
kbporter <kbporter@umich.edu>
2013-02-10 15:38:19 -0500

    split gui, lcm, and constraints into classes

--- java/src/LCMSend.java
+++ java/src/LCMSend.java
@@ -13,11 +13,24 @@ import april.*;
 
 public class LCMSend
 {
+    LCM lcm;
 
-    public LCMSend()
-    {
-        
-        
+    public LCMSend(){}
+    
+    void send(double[] angles){
+	long now = TimeUtil.utime();
+        dynamixel_command_list_t cmdlist = new dynamixel_command_list_t();
+        cmdlist.len = 6;
+        cmdlist.commands = new dynamixel_command_t[cmdlist.len];
+        for (int i = 0; i < 6; i++) {
+            dynamixel_command_t cmd = new dynamixel_command_t();
+            cmd.position_radians = MathUtil.mod2pi(angles[i]);
+            cmd.utime = now;
+            cmd.speed = 0.05;
+            cmd.max_torque = 0.6;
+            cmdlist.commands[i] = cmd;
+        }
+        lcm.publish("ARM_COMMAND", cmdlist);
     }
 
 }
